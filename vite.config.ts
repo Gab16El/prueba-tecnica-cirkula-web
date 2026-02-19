20268872ace2e64c5e264ce08d7b132410c3d2ab
Gab16El <noytte29@gmail.com>
2026-02-19 16:32:43 -0500

    feat: add relative paths and taiwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+    resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@features': path.resolve(__dirname, './src/core/features'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@helpers': path.resolve(__dirname, './src/helpers'),
+      '@router': path.resolve(__dirname, './src/router'),
+      '@api': path.resolve(__dirname, './src/api'),
+    }
+  }
 })
