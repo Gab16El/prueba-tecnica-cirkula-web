630126370a549e4622c418ba342454e2e9260d62
Gab16El <noytte29@gmail.com>
2026-02-19 23:19:02 -0500

    fix: add the url of the icon, shadow and retina for maker manually

--- src/core/features/map/LocationPicker.tsx
+++ src/core/features/map/LocationPicker.tsx
@@ -1,16 +1,14 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import { useMapEvents } from 'react-leaflet';
 
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconUrl: markerIcon,
-    shadowUrl: markerShadow,
-    iconRetinaUrl: markerIcon,
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
 });
-
 
 export const LocationPicker = ({ onSelect }: { onSelect: (lat: number, lng: number) => void }) => {
     useMapEvents({ click(e) { onSelect(e.latlng.lat, e.latlng.lng); } });
